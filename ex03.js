9f7e2f71a7fa8d175e65a1e3703631f720965a98
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-10-27 19:54:07 -0300

    doing ex03

---
+++ ex03.js
@@ -0,0 +1,31 @@
+function marcador_vida(vida_personagem, classe) {
+    var classe_1 = 20
+    var classe_2 = 30
+    var classe_3 = 40
+    var classe_4 = 50
+
+    if (vida_personagem <= 20) {
+        console.log('Você morreu!')
+    } else if(classe === classe_2) {
+        vida_atual = vida_personagem - classe_2
+        if (vida_atual <= 0){
+            console.log("Você morreu")
+        } else if (vida_atual > 0) {
+            console.log(vida_atual)
+        }
+    } else if(classe === classe_3) {
+        vida_atual = vida_personagem - classe_3
+        if (vida_atual <= 0){
+            console.log("Você morreu")
+        } else if (vida_atual > 0) {
+            console.log(vida_atual)
+        }
+    } else if(classe === classe_4) {
+        vida_atual = vida_personagem - classe_4
+        if (vida_atual <= 0){
+            console.log("Você morreu")
+        } else if (vida_atual > 0) {
+            console.log(vida_atual)
+        }
+    }   
+}
