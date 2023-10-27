bdb31438a0f46271f30d02af898d157e3d6c22fc
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-10-27 20:04:59 -0300

    turning console.log into return

--- ex04.js
+++ ex04.js
@@ -1,9 +1,9 @@
 function placar_partida(placar) {
     if (placar[0] > placar[1]) {
-        console.log("time A esta ganhando")
+        return "time A esta ganhando"
     } else if (placar[0] < placar[1]) {
-        console.log("time B esta ganhando")
+        return "time B esta ganhando"
     } else if (placar[0] === placar[1]) {
-        console.log("A partida esta empatada")
+        return "A partida esta empatada"
     }
 }
