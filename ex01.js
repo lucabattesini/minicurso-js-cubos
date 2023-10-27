c5edd24ca9bcf043ae393ab11ee82bced9bf112a
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-10-27 19:38:28 -0300

    correcting ex01

--- ex01.js
+++ ex01.js
@@ -1,15 +1,12 @@
-var peso_a1 = 1
-var peso_a2 = 3
-var peso_b1 = 4
-var peso_b2 = 2
+function balanca(peso_a1, peso_a2, peso_b1, peso_b2) {
+    var lado_a = peso_a1 + peso_a2
+    var lado_b = peso_b1 + peso_b2
 
-var lado_a = peso_a1 + peso_a2
-var lado_b = peso_b1 + peso_b2
-
-if (lado_a > lado_b) {
-    console.log('A balança se inclina para o lado A')
-} else if (lado_a < lado_b) {
-    console.log('A balança se inclina para o lado B')
-} else {
-    console.log('A balança não se inclina para nenhum lado')
+    if (lado_a > lado_b) {
+        console.log('A balança se inclina para o lado A')
+    } else if (lado_a < lado_b) {
+        console.log('A balança se inclina para o lado B')
+    } else {
+        console.log('A balança não se inclina para nenhum lado')
+    }
 }
